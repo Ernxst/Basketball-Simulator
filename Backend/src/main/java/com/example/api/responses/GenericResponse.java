d82f983d71ec8a4a7ed19ac2072711b58a8ff97a
Ernest Badu <ernxst@outlook.com>
2021-07-08 17:30:20 +0100

    Use HttpStatus type instead of String to store response codes

--- Backend/src/main/java/com/example/api/responses/GenericResponse.java
+++ Backend/src/main/java/com/example/api/responses/GenericResponse.java
@@ -1,17 +1,10 @@
 package com.example.api.responses;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-
-import java.util.List;
 
 public class GenericResponse {
     private String message;
     private HttpStatus code;
-    private String error;
 
     public GenericResponse(String message) {
         super();
@@ -28,20 +21,6 @@ public class GenericResponse {
     public GenericResponse(String message, HttpStatus code, String error) {
         this.message = message;
         this.code = code;
-        this.error = error;
-    }
-
-    public GenericResponse(List<FieldError> fieldErrors, List<ObjectError> globalErrors) {
-        super();
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            this.message = mapper.writeValueAsString(fieldErrors);
-            this.error = mapper.writeValueAsString(globalErrors);
-        } catch (JsonProcessingException e) {
-            this.message = "";
-            this.error += e.getMessage();
-        }
-        this.code = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
     public String getMessage() {
@@ -58,13 +37,5 @@ public class GenericResponse {
 
     public void setCode(HttpStatus code) {
         this.code = code;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
     }
 }

cff5b0a0f072d5d2aae9704d4c86119157865a12
Ernest Badu <ernxst@outlook.com>
2021-07-08 17:22:42 +0100

    Remove cmdline options for backend startup;

--- Backend/src/main/java/com/example/Application.java
+++ Backend/src/main/java/com/example/Application.java
@@ -6,22 +6,12 @@ TODO - Pass "b" flag from docker/python run script to here
 
 import com.example.api.AppLogger;
 import com.example.db.Database;
-import org.apache.commons.cli.*;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
 
 /**
  * Driver code to start RESTful API.
@@ -31,9 +21,9 @@ import java.util.Properties;
 @ComponentScan({"com.example.services", "com.example.config", "com.example.api"})
 @EntityScan("com.example.entities")
 @EnableJpaRepositories("com.example.repositories")
-public class Application implements ApplicationRunner {
+public class Application {
     // The port this backend runs on.
-    public static String PORT;
+    public static String PORT = "8100";
     // The port the frontend runs on.
     public static String APP_PORT = "8080";
     /**
@@ -42,69 +32,10 @@ public class Application implements ApplicationRunner {
     public static boolean broadcast;
 
     public static void main(String[] args) {
-        // Load properties file
-        Properties properties = new Properties();
-        try (InputStream is = Application.class.getResourceAsStream("../../application.properties")) {
-            properties.load(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String defaultPort = properties.getProperty("backend.app.port");
-
-        Options options = new Options();
-        Option portOption = new Option("p", "port", true,
-                "(optional) choose to run on a desired port - default is " + defaultPort);
-        portOption.setRequired(false);
-        options.addOption(portOption);
-
-        Option logOption = new Option("l", false, "(optional) include logging information during execution");
-        logOption.setRequired(false);
-        options.addOption(logOption);
-
-        Option broadcastOption = new Option("b", false, "(optional) broadcast IP address of this device to allow other devices to connect to the backend automatically. DO NOT USE ON PUBLIC NETWORKS!");
-        logOption.setRequired(false);
-        options.addOption(broadcastOption);
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("utility-name", options);
-            System.exit(1);
-            return;
-        }
-
-        String port = cmd.getOptionValue("port", defaultPort);
-        List<String> arguments = Arrays.asList(cmd.getArgs());
-        AppLogger.LOG_API_CALLS = arguments.contains("l");
-
-        Application.broadcast = arguments.contains("b");
-        AppLogger.welcome();
-        SpringApplication application = new SpringApplication(Application.class);
-
-        try {
-            // Just to ensure the port is a valid port.
-            int ignored = Integer.parseInt(port);
-            PORT = port;
-            application.setDefaultProperties(Collections.singletonMap("server.port", port));
-        } catch (NumberFormatException ignored) {
-            System.out.println("error: invalid port \"" + port + "\" - must be a number");
-            formatter.printHelp("utility-name", options);
-            System.exit(1);
-            return;
-        }
-        application.run(args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
         // Connect to cloud database.
         Database database = Database.getInstance();
         // Output startup logging information
         AppLogger.start();
+        SpringApplication.run(Application.class, args);
     }
 }

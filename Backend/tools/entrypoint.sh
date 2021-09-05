48f56cd0cd28c1c1183146874e844f50ac1b5089
Ernest Badu <ernxst@outlook.com>
2021-09-05 19:48:32 +0100

    Use docker-compose to create development database

--- Backend/tools/entrypoint.sh
+++ Backend/tools/entrypoint.sh
@@ -8,5 +8,13 @@ set -e
 #  echo "[ERROR] Application is not built" && exit 1
 #fi
 
+#if [[ "$ENV" != "production" ]]; then
+#  echo "== Waiting for Database =="
+#  while ! pg_isready -h db -p 5432 -q -U ${DB_USER}; do
+#    echo >&2 "Postgres DB is unavailable - waiting"
+#    sleep 1
+#  done
+#fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"

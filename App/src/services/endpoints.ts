bfbd2c6b8900308092dd719121624b8903d421ff
Ernest Badu <ernxst@outlook.com>
2021-09-05 16:06:21 +0100

    Update package.json scripts

--- App/src/services/endpoints.ts
+++ App/src/services/endpoints.ts
@@ -1,5 +1,9 @@
-const PORT: number = 8100;
-export const BASE_URL: string = `http://localhost:${PORT}/`;
+const getBaseUrl = (environment: string): string => {
+    const PORT: number = 8100;
+    return (environment == 'production') ? 'https://basketball-simulator-app.herokuapp.com/' :
+        `http://localhost:${PORT}/`;
+};
+export const BASE_URL: string = getBaseUrl('development');
 
 export const RELATIVE_USER_ENDPOINT: string = "user/";
 export const RELATIVE_USER_LOGIN_ENDPOINT: string = RELATIVE_USER_ENDPOINT + "login";
@@ -8,4 +12,3 @@ export const RELATIVE_USER_REGISTER_ENDPOINT: string = RELATIVE_USER_ENDPOINT + 
 export const FULL_USER_ENDPOINT: string = BASE_URL + RELATIVE_USER_ENDPOINT;
 export const FULL_USER_LOGIN_ENDPOINT: string = BASE_URL + RELATIVE_USER_LOGIN_ENDPOINT;
 export const FULL_USER_REGISTER_ENDPOINT: string = BASE_URL + RELATIVE_USER_REGISTER_ENDPOINT;
-

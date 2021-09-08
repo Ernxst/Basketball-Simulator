a3abe29cca363b68e21ae2a6bb3c3d1a532fffff
Ernest Badu <ernxst@outlook.com>
2021-09-08 23:07:55 +0100

    Fix tests

--- Backend/src/test/java/com/example/api/AbstractApiTest.java
+++ Backend/src/test/java/com/example/api/AbstractApiTest.java
@@ -1,119 +1,181 @@
 package com.example.api;
 
-import com.example.ApiTestConfig;
 import com.example.Application;
-import com.example.config.JwtTokenFilter;
 import com.example.config.JwtTokenUtil;
 import com.example.entities.user.User;
 import com.example.repositories.UserRepository;
-import com.example.services.user.UserService;
+import com.example.services.NameService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.sql.DataSource;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.matches;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ContextConfiguration(classes = {ApiTestConfig.class})
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AbstractApiTest {
-    protected final String TEST_USERNAME = "testuser";
+    protected final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    protected final JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
+    protected final ObjectMapper objectMapper = new ObjectMapper();
+    protected final TypeReference<Map<String, Object>> typeRef = new TypeReference<>() {
+    };
+    protected final String BAD_CREDENTIALS_MESSAGE = "Your username or password was incorrect, please try again.";
+
+    protected static final String TEST_USERNAME = "testuser";
     protected final String TEST_PASSWORD = "password";
     protected final String VALID_REGISTER_USERNAME = "test-register";
     protected final String VALID_REGISTER_PASSWORD = "test-password";
-    protected BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    protected Optional<User> testUser = Optional.of(new User(TEST_USERNAME, passwordEncoder.encode(TEST_PASSWORD)));
-    protected Optional<User> newUser = Optional.of(new User(VALID_REGISTER_USERNAME, passwordEncoder.encode(VALID_REGISTER_PASSWORD)));
-
-    @Autowired
-    protected AuthenticationManager authenticationManager;
-
-    @Autowired
-    private UserService userService;
-
-    @MockBean
-    private UserRepository userRepository;
-
-    @Autowired
-    private JwtTokenFilter jwtTokenFilter;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
+    protected final User testUser = new User(TEST_USERNAME, passwordEncoder.encode(TEST_PASSWORD));
+    protected final User newUser = new User(VALID_REGISTER_USERNAME, passwordEncoder.encode(VALID_REGISTER_PASSWORD));
+    protected final User registrationRequest = new User(VALID_REGISTER_USERNAME, VALID_REGISTER_PASSWORD);
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private DataSource dataSource;
+
+    @MockBean
+    private Connection connection;
+
+    @MockBean
+    private NameService nameService;
 
     @BeforeEach
-    private void createTestUser() {
-        Mockito.when(userRepository.findByUsername(TEST_USERNAME)).thenReturn(testUser);
-        Optional<User> registrationRequest = Optional.of(new User(VALID_REGISTER_USERNAME, VALID_REGISTER_PASSWORD));
-        Mockito.when(
-                userRepository.save(argThat(new UserMatcher(registrationRequest.get())))
-        ).thenReturn(newUser.get());
+    public void setup() {
+        Mockito.when(userRepository.findByUsername(TEST_USERNAME)).thenReturn(Optional.of(testUser));
+        String notTestUserMatcher = matches("^((?!" + TEST_USERNAME + ").)*$");
+        Optional<User> blankUser = Optional.empty();
+        Mockito.when(userRepository.findByUsername(notTestUserMatcher)).thenReturn(blankUser);
+        Mockito.when(userRepository.save(argThat(new UserMatcher(registrationRequest)))
+        ).thenReturn(newUser);
+    }
+
+    protected void expectFieldInResponse(MvcResult result, String key, String expectedMessage)
+            throws UnsupportedEncodingException, JsonProcessingException {
+        Map<String, Object> response = getResponse(result);
+        assertTrue(response.containsKey(key));
+        assertEquals(expectedMessage, response.get(key));
+    }
+
+    protected Map<String, Object> getResponse(MvcResult result)
+            throws UnsupportedEncodingException, JsonProcessingException {
+        String response = result.getResponse().getContentAsString();
+        return objectMapper.readValue(response, typeRef);
+    }
+
+    protected String toSnakeCase(String input) {
+        String ret = input.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2").replaceAll("([a-z])([A-Z])", "$1_$2");
+        return ret.toLowerCase();
+    }
+
+    protected Map<String, Object> toSnakeCaseMap(Map<String, Object> map) {
+        Map<String, Object> formatted = new HashMap<>();
+        map.forEach((String key, Object value) -> {
+            String formattedKey = toSnakeCase(key);
+            if (value instanceof String)
+                value = toSnakeCase((String) value);
+            else if (value instanceof Map)
+                value = toSnakeCaseMap((Map<String, Object>) value);
+            formatted.put(formattedKey, value);
+        });
+        return formatted;
     }
 
     private ResultActions testEndpoint(String contextPath, String endpoint, HttpMethod method,
-                                       Object requestBody, String expectedMessage, boolean auth) throws Exception {
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.request(method, contextPath + "/" + endpoint);
-        if (auth)
-            request = request.with(user(TEST_USERNAME));
-        request
-                .with(csrf())
+                                       Object requestBody, HttpStatus expectedStatus, boolean auth)
+            throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .request(method, contextPath + "/" + endpoint);
+        if (auth) {
+            String token = jwtTokenUtil.generateToken(testUser);
+            request = request.header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        }
+
+        if (!method.equals(HttpMethod.GET)) {
+            Map<String, Object> body = objectMapper.convertValue(requestBody, typeRef);
+            body = toSnakeCaseMap(body);
+            request = request.content(objectMapper.writeValueAsString(body));
+        }
+
+        request.with(csrf())
                 .secure(true)
-                .header("Access-Control-Request-Method", method.toString())
-                .header("Origin", "http://localhost:" + Application.APP_PORT)
-                .content(objectMapper.writeValueAsString(requestBody))
+                .header(HttpHeaders.ORIGIN, "http://localhost:" + Application.APP_PORT)
+                .header(HttpHeaders.CONNECTION, "keep-alive")
                 .characterEncoding(StandardCharsets.UTF_8.name())
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON);
 
-        return mockMvc.perform(request)
-                .andExpect(content().string(containsString(expectedMessage)));
+        return mockMvc.perform(request).andExpect(status().is(expectedStatus.value()));
     }
 
-    protected ResultActions testGetEndpoint(String contextPath, String endpoint, Object requestBody, String expectedMessage) throws Exception {
-        return testEndpoint(contextPath, endpoint, HttpMethod.GET, requestBody, expectedMessage, false);
+    protected ResultActions testGetEndpoint(String contextPath, String endpoint,
+                                            HttpStatus expectedStatus) throws Exception {
+        return testEndpoint(contextPath, endpoint, HttpMethod.GET, null, expectedStatus, false);
     }
 
-    protected ResultActions testAuthenticatedGetEndpoint(String contextPath, String endpoint, Object requestBody, String expectedMessage) throws Exception {
-        return testEndpoint(contextPath, endpoint, HttpMethod.GET, requestBody, expectedMessage, true);
+    protected ResultActions testAuthenticatedGetEndpoint(String contextPath, String endpoint,
+                                                         HttpStatus expectedStatus) throws Exception {
+        return testEndpoint(contextPath, endpoint, HttpMethod.GET, null, expectedStatus, true);
     }
 
-    protected ResultActions testPostEndpoint(String contextPath, String endpoint, Object requestBody, String expectedMessage) throws Exception {
-        return testEndpoint(contextPath, endpoint, HttpMethod.POST, requestBody, expectedMessage, false);
+    protected ResultActions testPostEndpoint(String contextPath, String endpoint, Object requestBody,
+                                             HttpStatus expectedStatus) throws Exception {
+        return testEndpoint(contextPath, endpoint, HttpMethod.POST, requestBody, expectedStatus, false);
     }
 
-    protected ResultActions testAuthenticatedPostEndpoint(String contextPath, String endpoint, Object requestBody, String expectedMessage) throws Exception {
-        return testEndpoint(contextPath, endpoint, HttpMethod.POST, requestBody, expectedMessage, true);
+    protected ResultActions testAuthenticatedPostEndpoint(String contextPath, String endpoint,
+                                                          Object requestBody, HttpStatus expectedStatus)
+            throws Exception {
+        return testEndpoint(contextPath, endpoint, HttpMethod.POST, requestBody, expectedStatus, true);
     }
 
-    protected ResultActions testDeleteEndpoint(String contextPath, String endpoint, Object requestBody, String expectedMessage) throws Exception {
-        return testEndpoint(contextPath, endpoint, HttpMethod.DELETE, requestBody, expectedMessage, false);
+    protected ResultActions testAuthenticatedDeleteEndpoint(String contextPath, String endpoint,
+                                                            Object requestBody, HttpStatus expectedStatus)
+            throws Exception {
+        return testEndpoint(contextPath, endpoint, HttpMethod.DELETE, requestBody, expectedStatus, true);
     }
 
-    protected ResultActions testAuthenticatedDeleteEndpoint(String contextPath, String endpoint, Object requestBody, String expectedMessage) throws Exception {
-        return testEndpoint(contextPath, endpoint, HttpMethod.DELETE, requestBody, expectedMessage, true);
+    protected ResultActions testAuthenticatedPatchEndpoint(String contextPath, String endpoint,
+                                                           Object requestBody, HttpStatus expectedStatus)
+            throws Exception {
+        return testEndpoint(contextPath, endpoint, HttpMethod.PATCH, requestBody, expectedStatus, true);
     }
 
     private static class UserMatcher implements ArgumentMatcher<User> {
@@ -125,7 +187,7 @@ public abstract class AbstractApiTest {
 
         @Override
         public boolean matches(User otherUser) {
-            return user.equals(otherUser);
+            return user.getUsername().equals(otherUser.getUsername());
         }
     }
 }
--- Backend/src/test/java/com/example/api/AccountControllerTest.java
+++ Backend/src/test/java/com/example/api/AccountControllerTest.java
@@ -1,121 +1,125 @@
 package com.example.api;
 
-import com.example.api.controllers.account.AccountController;
 import com.example.api.controllers.account.requests.AuthRequest;
 import com.example.api.controllers.account.requests.ChangePasswordRequest;
 import com.example.api.controllers.account.requests.DeleteUserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.io.UnsupportedEncodingException;
 import java.text.MessageFormat;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@WebMvcTest(AccountController.class)
 /*
  * Unit test for the /user/* endpoints.
  */
 public class AccountControllerTest extends AbstractApiTest {
 
+    private void expectJwtTokenInHeader(MvcResult result)
+            throws UnsupportedEncodingException, JsonProcessingException {
+        MockHttpServletResponse response = result.getResponse();
+        assertTrue(response.containsHeader(HttpHeaders.AUTHORIZATION));
+        String header = response.getHeader(HttpHeaders.AUTHORIZATION);
+        assertTrue(header.startsWith("Bearer "));
+        assertTrue(getResponse(result).containsKey("token"));
+    }
+
     @Test
     public void loginValidUsernameAndPassword() throws Exception {
-        // TODO: Expect JWT token to be returned in header
-        ResultActions result = testPostEndpoint("/user", "login",
-                new AuthRequest(TEST_USERNAME, TEST_PASSWORD), "Login success");
-        result.andExpect(status().isOk());
+        ResultActions result = testPostEndpoint("/users", "login",
+                new AuthRequest(TEST_USERNAME, TEST_PASSWORD), HttpStatus.CREATED);
         MvcResult mvcResult = result.andReturn();
+        expectJwtTokenInHeader(mvcResult);
     }
 
     @Test
     public void loginValidUsernameWrongPassword() throws Exception {
-        ResultActions result = testPostEndpoint("/user", "login",
-                new AuthRequest(TEST_USERNAME, "aaa"), "Login failed");
-        result.andExpect(status().isUnauthorized());
+        ResultActions result = testPostEndpoint("/users", "login",
+                new AuthRequest(TEST_USERNAME, "aaa"), HttpStatus.UNAUTHORIZED);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", BAD_CREDENTIALS_MESSAGE);
     }
 
     @Test
     public void loginNonExistingUsername() throws Exception {
-        ResultActions result = testPostEndpoint("/user", "login",
-                new AuthRequest("doesntexist", TEST_PASSWORD), "does not exist");
-        result.andExpect(status().isUnauthorized());
+        ResultActions result = testPostEndpoint("/users", "login",
+                new AuthRequest("doesntexist", TEST_PASSWORD), HttpStatus.UNAUTHORIZED);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", BAD_CREDENTIALS_MESSAGE);
     }
 
     @Test
     public void registerValidUsernameAndPassword() throws Exception {
-        // TODO: Expect JWT token to be returned in header
-        ResultActions result = testPostEndpoint("/user", "register",
-                new AuthRequest(VALID_REGISTER_USERNAME, VALID_REGISTER_PASSWORD),
-                "Registration success");
-        result.andExpect(status().isOk());
+        ResultActions result = testPostEndpoint("/users", "register",
+                new AuthRequest(VALID_REGISTER_USERNAME, VALID_REGISTER_PASSWORD), HttpStatus.CREATED);
         MvcResult mvcResult = result.andReturn();
+        expectJwtTokenInHeader(mvcResult);
     }
 
     @Test
     public void registerTakenUsername() throws Exception {
-        ResultActions result = testPostEndpoint("/user", "register",
-                new AuthRequest(TEST_USERNAME, "aaa"), "already in use");
-        result.andExpect(status().isConflict());
+        ResultActions result = testPostEndpoint("/users", "register",
+                new AuthRequest(TEST_USERNAME, "aaa"), HttpStatus.CONFLICT);
+        String expectedMessage = MessageFormat.format("The username {0} is already in use, please try another. Did you mean to sign in?", TEST_USERNAME);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", expectedMessage);
     }
 
     @Test
     public void changePasswordValidUsernameAndPassword() throws Exception {
-        ResultActions result = testAuthenticatedPostEndpoint("/user", "change-password",
-                new ChangePasswordRequest(TEST_PASSWORD, "some-password"),
-                "Success");
-        result.andExpect(status().isOk());
+        ResultActions result = testAuthenticatedPatchEndpoint("/users", TEST_USERNAME + "/change_password",
+                new ChangePasswordRequest(TEST_PASSWORD, "some-password"), HttpStatus.NO_CONTENT);
         MvcResult mvcResult = result.andReturn();
     }
 
     @Test
     public void changePasswordValidUsernameWrongPassword() throws Exception {
-        ResultActions result = testAuthenticatedPostEndpoint("/user", "change-password",
+        ResultActions result = testAuthenticatedPatchEndpoint("/users", TEST_USERNAME + "/change_password",
                 new ChangePasswordRequest("incorrect", "some-password"),
-                "Your password was incorrect, please try again.");
-        result.andExpect(status().isUnauthorized());
+                HttpStatus.UNAUTHORIZED);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", BAD_CREDENTIALS_MESSAGE);
     }
 
     @Test
     public void changePasswordWrongUsername() throws Exception {
         String username = "doesntexist";
-        ResultActions result = testAuthenticatedPostEndpoint("/user", "change-password",
-                new ChangePasswordRequest(TEST_PASSWORD, "some-password"),
-                MessageFormat.format("The user with username {0} cannot be found.", username));
-        result.andExpect(status().isUnauthorized());
+        ResultActions result = testAuthenticatedPatchEndpoint("/users", username + "/change_password",
+                new ChangePasswordRequest(TEST_PASSWORD, "some-password"), HttpStatus.UNAUTHORIZED);
+        String expectedMessage = MessageFormat.format("The user with username {0} cannot be found.", username);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", expectedMessage);
+
     }
 
     @Test
     public void deleteUserValidUsernameAndPassword() throws Exception {
-        ResultActions result = testAuthenticatedDeleteEndpoint("/user", TEST_USERNAME + "/delete",
-                new DeleteUserRequest(TEST_PASSWORD),
-                "Success");
-        result.andExpect(status().isOk());
+        ResultActions result = testAuthenticatedDeleteEndpoint("/users", TEST_USERNAME,
+                new DeleteUserRequest(TEST_PASSWORD), HttpStatus.NO_CONTENT);
         MvcResult mvcResult = result.andReturn();
     }
 
     @Test
     public void deleteUserValidUsernameWrongPassword() throws Exception {
-        ResultActions result = testAuthenticatedDeleteEndpoint("/user", TEST_USERNAME + "/delete",
-                new DeleteUserRequest("incorrect"),
-                "Your password was incorrect, please try again.");
-        result.andExpect(status().isUnauthorized());
+        ResultActions result = testAuthenticatedDeleteEndpoint("/users", TEST_USERNAME,
+                new DeleteUserRequest("incorrect"), HttpStatus.UNAUTHORIZED);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", BAD_CREDENTIALS_MESSAGE);
     }
 
     @Test
     public void deleteUserWrongUsername() throws Exception {
         String username = "doesntexist";
-        ResultActions result = testAuthenticatedDeleteEndpoint("/user", username + "/delete",
-                new DeleteUserRequest(TEST_PASSWORD),
-                MessageFormat.format("The user with username {0} cannot be found.", username));
-        result.andExpect(status().isUnauthorized());
+        ResultActions result = testAuthenticatedDeleteEndpoint("/users", username,
+                new DeleteUserRequest(TEST_PASSWORD), HttpStatus.UNAUTHORIZED);
+        String expectedMessage = MessageFormat.format("The user with username {0} cannot be found.", username);
         MvcResult mvcResult = result.andReturn();
+        expectFieldInResponse(mvcResult, "error", expectedMessage);
     }
-
 }

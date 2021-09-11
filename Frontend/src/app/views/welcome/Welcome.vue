6284fd19293a0d2d6154b6be983ce01464ebdb8c
Ernest Badu <ernxst@outlook.com>
2021-09-11 21:59:10 +0100

    Ensure text on input fields is always visible on mobile devices

--- Frontend/src/app/views/welcome/Welcome.vue
+++ Frontend/src/app/views/welcome/Welcome.vue
@@ -117,8 +117,12 @@ h1 {
     text-align: center;
 }
 
-.text-input label, .text-input input {
+.text-input label {
     color: #FFF;
+}
+
+.text-input input {
+    color: var(--black);
 }
 
 @media (min-width: 768px) {
@@ -146,6 +150,10 @@ h1 {
     .text-input .input-container {
         background: rgba(254, 254, 254, 0.075);
         border: 2px solid rgba(254, 254, 254, 0.3);
+    }
+
+    .text-input input {
+        color: #FFF;
     }
 }
 
